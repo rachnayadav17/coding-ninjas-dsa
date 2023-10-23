9ae249ab63d1849f2670dcd0072a61d372aecd2b
Rachna Yadav <rachna.tjr@gmail.com>
2023-10-23 23:07:17 +0530

    fiboonacii and average marks

---
+++ Introduction-to-python/week1/fibbonaci.py
@@ -0,0 +1,17 @@
+def fibonacci(n):
+    num1=0
+    num2=1
+    next_number = num2  
+    count = 1
+ 
+    while count <= n:
+        print(next_number, end=" ")
+        count += 1
+        num1, num2 = num2, next_number
+        next_number = num1 + num2
+    print() 
+
+
+
+n=int(input())
+fibonacci(n)
